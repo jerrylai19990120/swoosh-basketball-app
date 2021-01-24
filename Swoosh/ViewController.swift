f978770af44ab7cf9f6262bd1067677ce0ea175f
jerry <jerrylai19990120@gmail.com>
2021-01-24 16:38:44 -0500

    UI update

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
+    
+    
+    @IBOutlet weak var bgimg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
