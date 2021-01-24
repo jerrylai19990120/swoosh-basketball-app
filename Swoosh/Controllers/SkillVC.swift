db796257407d7081feae9ab6de684d363c61e04e
jerry <jerrylai19990120@gmail.com>
2021-01-24 18:57:06 -0500

    UI update

---
+++ Swoosh/Controllers/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  Swoosh
+//
+//  Created by Jerry Lai on 2021-01-24.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Swoosh/Controllers/ViewController.swift
+++ Swoosh/Controllers/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
 
 
 }
