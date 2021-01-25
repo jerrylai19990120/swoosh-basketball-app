c13fb89aa1afed45842489a201d57f975d6cf32e
jerry <jerrylai19990120@gmail.com>
2021-01-25 15:28:57 -0500

    update

--- Swoosh/Controllers/LeagueVC.swift
+++ Swoosh/Controllers/LeagueVC.swift
@@ -9,22 +9,48 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        player = Player()
+        nextBtn.isEnabled = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCsegue", sender: self)
     }
-    */
+    
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+            skillVC.player = player
+        }
+    }
 
 }
---
+++ Swoosh/Models/Player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  Swoosh
+//
+//  Created by Jerry Lai on 2021-01-25.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
